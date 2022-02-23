2263ea323caacb5a8a00cbe869573522453a0a62
Izzan Razak <83837106+zanceballos@users.noreply.github.com>
2022-02-23 13:25:40 +0800

    Update Hello Servlet java comment

--- src/main/java/first/webapp/HelloServlet.java
+++ src/main/java/first/webapp/HelloServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class HelloServlet
+ * Servlet implementation class HelloServlet HOHOHO NOOO
  */
 @WebServlet("/HelloServlet")
 public class HelloServlet extends HttpServlet {
